dd8824437a0b44f4b72432b7905d0b802cec27af
tsamarahlubis <124503782+tsamarahlubis@users.noreply.github.com>
2024-06-16 10:57:00 +0700

    Delete framework directory

--- framework/views/096f6ccaba23177ae78408e6ac26e600.php
+++
@@ -1,5 +0,0 @@
-    <div class="position-sticky" style="top: 5rem;">
-        <?php echo $__env->make('frontend.layouts.sidebar_recent_post', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-        <?php echo $__env->make('frontend.layouts.sidebar_archive', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    </div>
-<?php /**PATH C:\kuliah\UAS PEMWEB\lara-starter\resources\views/frontend/layouts/sidebar.blade.php ENDPATH**/ ?>
--- framework/views/f09fd719d8f75e71f8eecf9240fe072c.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('frontend.home.latest_post', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('frontend.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\kuliah\UAS PEMWEB\lara-starter\resources\views/frontend/home/index.blade.php ENDPATH**/ ?>
