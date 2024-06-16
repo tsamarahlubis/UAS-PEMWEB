672017633c5e539f9b87e480db486bd8ae08fb17
tsamarahlubis <124503782+tsamarahlubis@users.noreply.github.com>
2024-06-16 10:22:03 +0700

    Add files via upload

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+  <strong>Copyright © 2024 Tsamarah.</strong>
+  All rights reserved.
+  <div class="float-right d-none d-sm-inline-block" bis_skin_checked="1">
+    <b>Template Version</b> 3.2.0
+  </div>
+</footer>
